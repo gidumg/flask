78680e786f8145b767fa51ea86dae7358186101d
gidumg <129565532+gidumg@users.noreply.github.com>
2023-06-22 21:37:37 +0900

    final commit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from urllib.parse import quote_plus
 import pandas as pd
 from IPython.display import display, HTML
 import pymysql
+from sqlalchemy import text
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -42,15 +43,54 @@ def upload_file():
 
 
 def search_based_on_model(model_name):
+
+    password = quote_plus('!!@Ll752515')
     # SQL에서 모델 정보 가져오기
+
     engine = create_engine(f'mysql+pymysql://fred:{password}@fred1234.synology.me/fred')
-    query = f"""select * from comsmart_web where `모델명` = '{model_name}'"""
-    df = pd.read_sql(query, con=engine)
+    # 완벽하게 일치하는 경우 찾기
+    query = text("SELECT * FROM comsmart_web WHERE `주문코드` = :model_name")
+    items = pd.read_sql_query(query, con=engine, params={'model_name': model_name})
+
+    if items.empty:
+        query = text("SELECT * FROM comsmart_web WHERE `주문코드` LIKE :model_name")
+        items = pd.read_sql_query(query, con=engine, params={'model_name': model_name+'%'})
+
+
+    find_items2 = items[['주문코드', '바코드', '상품명', '총재고(H)', '허브매장(U)','본사재고(B)','판매가','도매가','파트너가', ' 상품이미지1' ]]
+
+
+    # 각 이미지 링크를 HTML 이미지 태그로 변환
+    find_items2[' 상품이미지1'] = find_items2[' 상품이미지1'].apply(lambda x: f'<img src="{x}" width="100">')
+    # 데이터프레임을 HTML로 변환
 
     # 데이터프레임을 HTML로 변환
-    html = df.to_html()
+    html = find_items2.to_html(escape=False)
 
-    return Markup(html)
+    css_style = """
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        tr:hover {
+            background-color: #ddd;
+        }
+    </style>
+    """
+
+    # CSS 스타일을 HTML에 적용합니다.
+    html_with_style = css_style + html
+
+    return Markup(html_with_style)
 
 
 @app.route('/search', methods=['POST'])
